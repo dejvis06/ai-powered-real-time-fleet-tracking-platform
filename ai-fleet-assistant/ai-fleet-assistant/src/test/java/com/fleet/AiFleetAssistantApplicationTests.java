dd33cf9f946cda6c10087c4d719f19112783e926
Dejvis Beqiraj <dejvis.beqiraj@lhind.dlh.de>
2026-07-22 18:47:50 +0200

    Initial commit

---
+++ ai-fleet-assistant/ai-fleet-assistant/src/test/java/com/fleet/AiFleetAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFleetAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
